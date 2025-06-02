abbca8525e0194cd900bc48b47cda08ee2ac8e8c
DJIBM02 <nouroudineibrahim15@gmail.com>
2025-06-02 01:07:43 +0100

    Update Role.java

--- src/main/java/com/securetalk/model/Role.java
+++ src/main/java/com/securetalk/model/Role.java
@@ -1,4 +1,25 @@
 package com.securetalk.model;
 
-public class Role {
+/**
+ * Énumération des rôles utilisateur
+ */
+public enum Role {
+    USER("USER"),
+    ADMIN("ADMIN"),
+    MODERATOR("MODERATOR");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
