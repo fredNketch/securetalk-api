4d1013e4ab58940539ce8104b8995e3296c14ae7
DJIBM02 <nouroudineibrahim15@gmail.com>
2025-06-02 01:11:40 +0100

    Update AuditableEntity.java

--- src/main/java/com/securetalk/model/AuditableEntity.java
+++ src/main/java/com/securetalk/model/AuditableEntity.java
@@ -1,4 +1,32 @@
 package com.securetalk.model;
 
-public class AuditableEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * Entité de base avec support de l'audit
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class AuditableEntity extends BaseEntity {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name = "created_ip", length = 45, updatable = false)
+    private String createdIp;
+
+    @Column(name = "modified_ip", length = 45)
+    private String modifiedIp;
 }
